9341e4fe1b941e35e3c8f4fd5036b58d8e3d00ab
Bakkoush <149477244+Bakkoush@users.noreply.github.com>
2025-11-12 23:59:19 +0000

    commit

--- routes/books.js
+++ routes/books.js
@@ -27,5 +27,29 @@ router.get('/addbook', function(req, res, next) {
     res.render('addbook'); // renders addbook.ejs
 });
 
+    // search_result route
+router.get('/search_result', function (req, res, next) {
+    const keyword = req.query.search_text; // matches the form input name
+
+    if (!keyword) {
+        return res.render("search_result.ejs", { searchTerm: "", results: [] });
+    }
+
+    const sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    const searchValue = "%" + keyword + "%";
+
+    db.query(sqlquery, [searchValue], (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.render("search_result.ejs", {
+                searchTerm: keyword,
+                results: result
+            });
+        }
+    });
+});
+
+
 // Export the router object so index.js can access it
 module.exports = router
